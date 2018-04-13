d3a3ecd65b18e11b6b01013c5e6627934518aad2
Łukasz Gandecki <lgandecki@css.edu>
2018-04-13 20:16:09 +0400

    working integration tests for mobile

---
+++ testing/mobile/integration/App.test.js
@@ -0,0 +1,25 @@
+/* eslint-env jest */
+import React from "react";
+import { render, wait } from "react-testing-library";
+import { ApolloProvider } from "react-apollo";
+import gqlClient from "../../common/gqlClient";
+import listsResolvers from "../../../server/src/api/graphql/lists/listsResolvers";
+import getListsWithDefaults from "../../common/getListsWithDefaults";
+import AppWithApollo from "../../../mobile/AppWithApollo";
+
+test("Rendering component connected to the server", async () => {
+  const listsRepository = await getListsWithDefaults();
+  const { getByText } = render(
+    <ApolloProvider
+      client={gqlClient({
+        context: { listsRepository },
+        resolvers: [listsResolvers]
+      })}
+    >
+      <AppWithApollo />
+    </ApolloProvider>
+  );
+
+  expect(getByText("loading")).toBeDefined();
+  await wait(() => getByText("second list"), { timeout: 500 });
+});
