598f3adba3d564380d3ce645b9f0cd92e04d7fbb
Łukasz Gandecki <lgandecki@css.edu>
2018-04-14 13:12:08 +0400

    updated circleci - run all the integration tests together, clean up

--- mobile/AppWithApollo.js
+++ mobile/AppWithApollo.js
@@ -9,12 +9,7 @@ const PureApp = props => {
     return <Text>Loading...</Text>;
   }
   return (
-    <View>
-      {Lists.map(list => <Text key={list.name}>{list.name}</Text>)}
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Changes you make will automatically reload.</Text>
-      <Text>Shake your phone to open the developer menu.</Text>
-    </View>
+    <View>{Lists.map(list => <Text key={list.name}>{list.name}</Text>)}</View>
   );
 };
 
